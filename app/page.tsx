dfa29cca5691e26044df5a1b35289bd315e851e9
Jonathan Ikpen <jonathanikpen4@gmail.com>
2023-11-07 15:38:20 +0000

    second commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
+
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="flex items-end gap-2 mb-7">ClassRoom App</div>
+    </div>
+  );
+}
