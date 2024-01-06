1641f178097ba5d703d3015f91ee9fe6c10ee0aa
Jonathan Ikpen <jonathanikpen4@gmail.com>
2024-01-06 22:03:16 +0100

    fix: removed firebase auth

--- app/(auth)/(routes)/auth/components/google-auth.tsx
+++ app/(auth)/(routes)/auth/components/google-auth.tsx
@@ -2,47 +2,16 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
-import { auth } from "@/lib/firebase";
-import { useSignInWithGoogle, useAuthState } from "react-firebase-hooks/auth";
 import toast from "react-hot-toast";
 
 const GoogleAuth = () => {
   const router = useRouter();
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-  const [userAuth] = useAuthState(auth);
-
-  if (userAuth) {
-    console.log("loggedin");
-  } else {
-    //  if (currentUser?.data?.member?.id === userAuth?.uid) {
-    //    router.push("/app");
-    //  }
-    // console.log("Not logged in");
-  }
 
   const handleAuth = async () => {
     try {
-      await signInWithGoogle();
-
-      const userData = {
-        userId: userAuth?.uid ?? "",
-        name: userAuth?.displayName ?? "",
-        email: userAuth?.email ?? "",
-        photoUrl: userAuth?.photoURL ?? "",
-      };
-
-      console.log(userData);
       toast.success("signed in successfully");
       router.push("/auth/new");
 
-      // addMember(userData, {
-      //   onSuccess: () => {
-      //     router.push("/onboarding");
-      //   },
-      //   onError: (error) => {
-      //     console.log(error);
-      //   },
-      // });
     } catch (error) {
       console.log(error);
       toast.error("error: " + error);
@@ -57,7 +26,7 @@ const GoogleAuth = () => {
         variant={"secondary"}
         className="w-full flex items-center gap-x-3"
         size={"lg"}
-        disabled={loading}
+        disabled={false}
       >
         <FcGoogle />
         Google
--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,20 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "classroom-app-5b3e1.firebaseapp.com",
-  projectId: "classroom-app-5b3e1",
-  storageBucket: "classroom-app-5b3e1.appspot.com",
-  messagingSenderId: "126855485559",
-  appId: "1:126855485559:web:988fef22aa72bacf1e8e72",
-  measurementId: "G-S2597D52W2",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth };
-
-// const analytics = getAnalytics(app);
+// import { initializeApp } from "firebase/app";
+// import { GoogleAuthProvider, getAuth } from "firebase/auth";
+//
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: "classroom-app-5b3e1.firebaseapp.com",
+//   projectId: "classroom-app-5b3e1",
+//   storageBucket: "classroom-app-5b3e1.appspot.com",
+//   messagingSenderId: "126855485559",
+//   appId: "1:126855485559:web:988fef22aa72bacf1e8e72",
+//   measurementId: "G-S2597D52W2",
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+//
+// export { auth };
+//
+// // const analytics = getAnalytics(app);
